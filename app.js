e6fd6c91a582e78a099f3e95f74786124bde43aa
vidhyarth patel <vidyarthpatel@gmial.com>
2024-05-26 21:53:31 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+let notesContainer = document.querySelector('.notes-container')
+let createBtn = document.querySelector('.btn')
+let notes = document.querySelectorAll('.input-box')
+
+createBtn.addEventListener('click', () => {
+    let p = document.createElement('p')
+    let img = document.createElement('img')
+    p.className = 'input-box'
+    img.src = "https://static-00.iconduck.com/assets.00/delete-icon-1864x2048-bp2i0gor.png"
+    p.setAttribute('contenteditable', 'true')
+    p.style.backgroundColor = "#0011"
+    notesContainer.append(p)
+    p.append(img)
+})
+
+notesContainer.addEventListener('click' , (eve) => {
+    if(eve.target.tagName === 'IMG'){
+        eve.target.parentElement.remove()
+    }
+})
